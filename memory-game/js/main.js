7ae9ec160cdfba93b60a018061243de812a927aa
George Jones <georgejones@Georges-MacBook-Pro-2.local>
2020-03-09 12:28:39 +0000

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -3,18 +3,24 @@ console.log("Up and Running!!!");
 const cards = ["Queen", "Queen", "King", "King"];
 let cardsInPlay = [];
 
-const cardOne = cards[0];
-const cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User Flipped " + cardsInPlay)
-
-if(cardsInPlay.length === 2){
+function checkForMatch(){
   if(cardsInPlay[0] === cardsInPlay[1]){
     alert("You Found A Match")
   }else{
     alert("Sorry Not A Match")
   }
 }
+
+function flipCard(cardId){
+
+  cardsInPlay.push(cards[cardId]);
+
+  console.log("User Flipped " + cards[cardId]);
+
+  if(cardsInPlay.length === 2){
+    checkForMatch();
+  }
+}
+
+flipCard(0)
+flipCard(2)
